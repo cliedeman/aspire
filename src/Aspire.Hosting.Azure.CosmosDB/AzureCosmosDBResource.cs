2f55f4d475c82d0171d18adc1a5b66ddd084f32e
Ciaran Liedeman <ciaran@stackworx.io>
2026-02-18 11:41:20 +0200

    fix: Use http scheme when using preview emulator

--- src/Aspire.Hosting.Azure.CosmosDB/AzureCosmosDBResource.cs
+++ src/Aspire.Hosting.Azure.CosmosDB/AzureCosmosDBResource.cs
@@ -88,7 +88,7 @@ public class AzureCosmosDBResource(string name, Action<AzureResourceInfrastructu
 
     internal bool IsPreviewEmulator =>
         this.TryGetContainerImageName(out var imageName) &&
-        imageName == $"{CosmosDBEmulatorContainerImageTags.Registry}/{CosmosDBEmulatorContainerImageTags.Image}:{CosmosDBEmulatorContainerImageTags.TagVNextPreview}";
+        imageName.StartsWith($"{CosmosDBEmulatorContainerImageTags.Registry}/{CosmosDBEmulatorContainerImageTags.Image}:{CosmosDBEmulatorContainerImageTags.TagVNextPreview}");
 
     /// <summary>
     /// Gets the account endpoint URI expression for the Cosmos DB account.
